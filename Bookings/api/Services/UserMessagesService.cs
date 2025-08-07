9cd79d2b2d3a697277ee55f1570aace4c277eeb0
Rioghan <cunniffe.rioghan@gmail.com>
2025-08-08 00:46:02 +0100

    Update UserMessagesService.cs

--- Bookings/api/Services/UserMessagesService.cs
+++ Bookings/api/Services/UserMessagesService.cs
@@ -17,7 +17,8 @@ namespace BookingsApi.Services
 
         public async Task<string> GetUserHasMessagesAsync()
         {
-            using var client = await _loginService.GetAuthenticatedClientAsync();
+            var client = await _loginService.GetAuthenticatedClientAsync();
+            await new LoginHelper4().GetLoggedInRequestAsync(client);
             var param = new Dictionary<string, string>
             {
                 { "siteCallback", "MemberCallback" },
@@ -33,7 +34,8 @@ namespace BookingsApi.Services
 
         public async Task<string> GetUserMessagesAsync(bool markAsRead = false, bool showExpired = false, bool showRead = true)
         {
-            using var client = await _loginService.GetAuthenticatedClientAsync();
+            var client = await _loginService.GetAuthenticatedClientAsync();
+            await new LoginHelper4().GetLoggedInRequestAsync(client);
             var baseUrl = "https://clubmanager365.com/ActionHandler.ashx";
 
             var parameters = new Dictionary<string, string>
@@ -54,7 +56,8 @@ namespace BookingsApi.Services
 
         public async Task<string> GetSentUserMessagesAsync()
         {
-            using var client = await _loginService.GetAuthenticatedClientAsync();
+            var client = await _loginService.GetAuthenticatedClientAsync();
+            await new LoginHelper4().GetLoggedInRequestAsync(client);
             var baseUrl = "https://clubmanager365.com/ActionHandler.ashx";
             var parameters = new Dictionary<string, string>
             {
